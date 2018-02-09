73984c13086c554d449be3284114b4e72e50c579
mingkaijiang <shanyujordan512@gmail.com>
2018-02-10 08:09:18 +1100

    commited the trend

---
+++ dataset_trend_comparison.R
@@ -0,0 +1,33 @@
+### Compare trends across datasets
+
+### inDF
+inDF <- read.csv("result/trend_comparison.csv")
+
+inDF1 <- subset(inDF, select = c("year", "nce_fixed_lulcc","nce_cum_fixed_lulcc",
+                                 "nep_fixed_lulcc", "nep_cum_flixed_lulcc"))
+
+inDF2 <- subset(inDF, select = c("year", "nce", "nce_cum_old_tem", "nep_old_tem",
+                                 "nep_cum_old_tem"))
+
+inDF3 <- subset(inDF, select = c("year", "nce_old_hurtt", "nce_cum_old_hurtt",
+                                 "nep_old_hurtt", "nep_cum_old_hurtt"))
+
+inDF1$dataset <- "fixed_lulc"
+inDF2$dataset <- "old_tem"
+inDF3$dataset <- "old_hurtt"
+
+colnames(inDF3) <- colnames(inDF2) <- colnames(inDF1) <- c("year","nce","nce_cum",
+                                                           "nep", "nep_cum","dataset")
+
+myDF <- rbind(inDF1, inDF2)
+myDF <- rbind(myDF, inDF3)
+
+### library
+require(ggplot2)
+
+### plotting
+p <- ggplot(myDF, aes(x=year, y=nce, group=dataset)) +
+    geom_line(aes(color=dataset))
+plot(p)
+
+
